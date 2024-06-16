955f4fd12404f638d55e4fa83495da9016d14a6a
ElenaZ-12 <166631491+ElenaZ-12@users.noreply.github.com>
2024-06-16 14:54:11 -0400

    intro adjustments

--- Intro.java
+++ Intro.java
@@ -17,14 +17,15 @@ public class Intro extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
+
         Label story1 = new Label("You¡¯re suffering from kitty cancer,\nmake 150 dollars by the end of the \nweek to pay for your medical fee!", textSize);
         addObject(story1,300,70);
-        
+
         Cancer cancer = new Cancer();
-        addObject(cancer,300,270);
+        addObject(cancer,300,280);
+        prepare();
     }
-    
+
     public void act()
     {
         if (Greenfoot.mouseClicked(null))
@@ -32,5 +33,14 @@ public class Intro extends World
             Game currentWorld = new Game();
             Greenfoot.setWorld(currentWorld);
         }
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Label label = new Label("<Click anywhere to proceed>", 30);
+        addObject(label,300,140);
     }
 }
